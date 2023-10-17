17c9b1dd0597a1b30daa2eaf3c0e5f380dbcce7a
MidMonkey <m265124@usna.edu>
2023-10-17 09:26:40 -0400

    test

--- game.py
+++ game.py
@@ -1,2 +1,34 @@
-print('hello world')
-print('hello world again')
+import pygame
+
+# pygame setup
+pygame.init()
+# Set window resolution
+WIDTH = 800
+HEIGHT = 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+running = True
+dt = 0
+
+background = pygame.Surface((WIDTH, HEIGHT,))
+background.fill((0, 0, 255))
+while running:
+    # poll for events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for frame rate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
