cf22f95ba5472228e7fac07cf1d46f09883cc86f
MidMonkey <m265124@usna.edu>
2023-10-21 18:09:19 -0400

    Added sand background, seaweed and a fish

--- game.py
+++ game.py
@@ -1,13 +1,21 @@
 import pygame
-import sys
+from random import randint
 
 # pygame setup
 pygame.init()
 # Set window resolution
 WIDTH = 1080
-HEIGHT = 720
+HEIGHT = 650
 sand_height = 100
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+sand = pygame.image.load("Fishpics/fishTile_021.png")
+weed = pygame.image.load("Fishpics/fishTile_030.png")
+picwidth= sand.get_width()
+picheight = sand.get_height()
+wh = randint(picheight, 2 * picheight)
+ww = randint(0, WIDTH)
+nwh = randint(picheight, 2 * picheight)
+nww = randint(0, WIDTH)
 background = pygame.Surface((WIDTH, HEIGHT))
 background.fill((52, 140, 235))
 clock = pygame.time.Clock()
@@ -23,9 +31,15 @@ while running:
     pygame.draw.rect(background, (204, 179, 129), (0, HEIGHT - sand_height, WIDTH, sand_height))
     screen.blit(background, (0, 0))
     sand = pygame.image.load("Fishpics/fishTile_021.png")
-    picwidth= sand.get_width()
-    picheight = sand.get_height()
-    screen.blit(sand, (WIDTH-picwidth,HEIGHT-picheight))
+    weed = pygame.image.load("Fishpics/fishTile_030.png")
+
+    for i in range(1,100):
+        screen.blit(sand, (WIDTH-i*picwidth,HEIGHT-picheight))
+    screen.blit(weed, (WIDTH - ww, HEIGHT - wh))
+    screen.blit(weed, (WIDTH - nww, HEIGHT - nwh))
+
+
+
     # pygame.QUIT event means the user clicked X to close your window
     # fill the screen with a color to wipe away anything from last frame
     # flip() the display to put your work on screen
